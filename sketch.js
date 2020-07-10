f4ea8af24d1d3df974aa8dcb2ab5b2fbab7e67f4
Ishita <ishitatrishasingh@gmail.com>
2020-07-09 19:43:42 -0700

    new box is created

--- sketch.js
+++ sketch.js
@@ -36,6 +36,8 @@ function setup(){
 
     bird = new Bird(100,100);
 
+    box6 = new Box(900,200,30,30)
+
 }
 
 function draw(){
@@ -61,4 +63,5 @@ function draw(){
 
     bird.display();
     platform.display();
+    box6.display();
 }
